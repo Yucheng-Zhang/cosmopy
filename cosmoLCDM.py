b84c0e6720c262c4e54ced59a80521f68308edbc
Yucheng Zhang <yczhang16807@icloud.com>
2019-06-16 17:51:58 -0400

    add cosmoLCDM and utils

--- cosmoLCDM.py
+++ cosmoLCDM.py
@@ -1,3 +1,50 @@
 '''
 LambdaCDM class.
 '''
+from astropy.cosmology import FlatLambdaCDM
+import camb
+
+Z_CMB = 1100
+
+
+class cosmoLCDM:
+    '''FlatLambdaCDM cosmology.'''
+
+    def __init__(self, H0, Om0, Ob0, Tcmb0, As, ns):
+        self.H0 = H0
+        self.h = self.H0 / 100.
+        self.Om0 = Om0
+        self.Ob0 = Ob0
+        self.Tcmb0 = Tcmb0
+        self.As = As
+        self.ns = ns
+
+        self.cosmo = FlatLambdaCDM(
+            H0=self.H0, Tcmb0=self.Tcmb0, Om0=self.Om0, Ob0=self.Ob0)
+
+    def H_z(self, z):
+        '''Hubble parameter at redshift z, i.e. H(z)'''
+        return self.H0 * self.cosmo.efunc(z)
+
+    def z2chi(self, z):
+        '''Get comoving distance in [Mpc] from redshift'''
+        return self.cosmo.comoving_distance(z).value
+
+    def w_z(self, z, zs=Z_CMB):
+        '''Lensing kernel.'''
+        chi_z = self.z2chi(z)
+        chi_zs = self.z2chi(zs)
+        return chi_z * (1. - chi_z / chi_zs)
+
+    def gen_pk(self, kmax, z1, z2):
+        '''Generate matter power spectrum Pmm(z, k).'''
+        pars = camb.CAMBparams()
+        pars.set_cosmology(H0=self.H0, ombh2=self.Ob0*self.h**2,
+                           omch2=(self.Om0-self.Ob0)*self.h**2, TCMB=self.Tcmb0)
+        pars.InitPower.set_params(As=self.As, ns=self.ns)
+
+        self.pk = camb.get_matter_power_interpolator(pars, zmin=z1-0.1, zmax=z2+0.1,
+                                                     kmax=kmax, nonlinear=True,
+                                                     hubble_units=False, k_hunit=False)
+
+        print('>> Matter power spectrum function self.pk.P(z,k) generated with CAMB.')
---
+++ utils.py
@@ -0,0 +1,19 @@
+'''
+Some useful functions.
+'''
+import numpy as np
+import scipy.interpolate as spi
+
+
+def gen_fg_z(dataz, z1, z2, bins=200):
+    '''Generate the redshift distribution function fg(z).'''
+    hist, bin_edges = np.histogram(
+        dataz, bins=bins, range=(z1, z2), density=True)
+    bin_mids = (bin_edges[:-1] + bin_edges[1:]) / 2.
+    xs = np.concatenate(([z1], bin_mids, [z2]))
+    ys = np.concatenate(([hist[0]], hist, [hist[-1]]))
+
+    fg_z = spi.interp1d(xs, ys, kind='linear',
+                        bounds_error=False, fill_value=(0., 0.))
+
+    return fg_z
