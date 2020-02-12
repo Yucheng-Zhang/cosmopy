dcf2f875bef71a41a9d0799069416aa383f342b1
Yucheng Zhang <ustczyc@outlook.com>
2020-02-12 16:56:20 -0500

    kind in fg

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ import numpy as np
 import scipy.interpolate as spi
 
 
-def gen_fg_z(dataz, z1, z2, bins=200):
+def gen_fg_z(dataz, z1, z2, bins=200, kind='linear'):
     '''Generate the redshift distribution function fg(z).'''
     hist, bin_edges = np.histogram(
         dataz, bins=bins, range=(z1, z2), density=True)
@@ -13,7 +13,7 @@ def gen_fg_z(dataz, z1, z2, bins=200):
     xs = np.concatenate(([z1], bin_mids, [z2]))
     ys = np.concatenate(([hist[0]], hist, [hist[-1]]))
 
-    fg_z = spi.interp1d(xs, ys, kind='linear',
+    fg_z = spi.interp1d(xs, ys, kind=kind,
                         bounds_error=False, fill_value=(0., 0.))
 
     print('>> Redshift distribution fg genertaed from data.')
